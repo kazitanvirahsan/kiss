42dbfb9f13b87342e512661869fedb9e25e50eb9
Kazi <ktanvirahsan@gmail.com>
2014-02-26 23:15:44 -0500

    kiss simple button design/adding mixin for general button group

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+       <meta charset="utf-8">
+       <title>Kiss- A simple CSS3 framework with Sass</title>
+<link href="/kiss/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<link href="/kiss/stylesheets/print.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<!--[if IE]>
+<link href="/stylesheets/ie.css" media="screen, projection" rel="stylesheet" type="text/css" />
+<![endif]-->     
+   </head>
+<body>
+    <button class='kiss-button-simple'>Submit</button>
+</body>
+</html>
 <?php
+
+?>
